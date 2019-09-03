31f0be8275cc56d44b4b1630d7df25ec738c4ff9
nestoraguilera <nestoraguilera@gmail.com>
2019-09-02 21:32:02 -0400

    Punto de partida

---
+++ PeriodosPerdidos/src/main/java/com/naao/PeriodosPerdidosApplication.java
@@ -0,0 +1,13 @@
+package com.naao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeriodosPerdidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeriodosPerdidosApplication.class, args);
+	}
+
+}
---
+++ PeriodosPerdidos/src/test/java/com/naao/PeriodosPerdidosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.naao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PeriodosPerdidosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
